c4f7e2a988b36c377a294095c6bb33902ba15f8b
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-03 21:20:43 +0800

    web

---
+++ JavaWeb20230803/src/main/java/servlet/TimeServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/servlet/time")
+public class TimeServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.print("Time: " + new Date());
+		out.close();
+		
+	}
+	
+}
