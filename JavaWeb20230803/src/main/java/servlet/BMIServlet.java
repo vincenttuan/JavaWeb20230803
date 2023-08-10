6903014d364b535ca6a925fceab57a32cb7d105f
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-10 19:54:42 +0800

    web

--- JavaWeb20230803/src/main/java/servlet/BMIServlet.java
+++ JavaWeb20230803/src/main/java/servlet/BMIServlet.java
@@ -40,6 +40,40 @@ public class BMIServlet extends HttpServlet {
 		out.print("BMI = " + bmi);
 		
 	}
+
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// 1. 設定編碼
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=UTF-8"); // 給瀏覽器看的
+		
+		PrintWriter out = resp.getWriter();
+		
+		// 2. 取得 h, w 的參數
+		String hStr = req.getParameter("h");
+		String wStr = req.getParameter("w");
+		
+		if(hStr == null || wStr == null) {
+			// 回應內容
+			out.print("請在網址列上輸入身高與體重");
+			return;
+		}
+		
+		// 3. 計算 BMI
+		double h = Double.parseDouble(hStr);
+		double w = Double.parseDouble(wStr);
+		double bmi = w / Math.pow(h/100, 2);
+		
+		// 4. 回應內容
+		out.print("BMI = " + bmi);
+
+		
+	}
+	
+	
 	
 	
 }
