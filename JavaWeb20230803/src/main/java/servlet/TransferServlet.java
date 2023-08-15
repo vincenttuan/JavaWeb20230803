e2b281cdefb87c7eb2558c6d8fd29907600558f9
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-15 19:39:33 +0800

    web

--- JavaWeb20230803/src/main/java/servlet/TransferServlet.java
+++ JavaWeb20230803/src/main/java/servlet/TransferServlet.java
@@ -1,5 +1,38 @@
 package servlet;
 
-public class TransferServlet {
+import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import service.TransferService;
+
+@WebServlet(urlPatterns = "/servlet/transfer")
+public class TransferServlet extends HttpServlet {
+	
+	// 建立翻譯服務物件
+	private TransferService transferService = new TransferService();
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String word = req.getParameter("word"); // 得到要翻譯的英文
+		
+		// (M) 透過翻譯服務物件來得到所需要的結果
+		String result = transferService.transfer(word); // 得到翻譯的中文結果
+		
+		// (V) 建立分派器並指向指定 jsp (view)
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/transferresult.jsp");
+		// 建立要傳遞給 jsp 的資料
+		req.setAttribute("word", word);
+		req.setAttribute("result", result);
+		// 傳遞
+		rd.forward(req, resp);
+	}
+	
+	
+	
 }
