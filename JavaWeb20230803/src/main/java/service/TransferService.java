111d18db90e80f96cea3c9a39e0efaa6cc5d32be
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-15 19:29:56 +0800

    java

--- JavaWeb20230803/src/main/java/service/TransferService.java
+++ JavaWeb20230803/src/main/java/service/TransferService.java
@@ -1,5 +1,21 @@
 package service;
 
+// 翻譯服務
 public class TransferService {
-
+	
+	public String transfer(String word) {
+		
+		switch (word) {
+			case "book":
+				return "書";
+			case "pen":
+				return "筆";
+			case "apple":
+				return "蘋果";
+			default:
+				return "我不知道";
+		}
+		
+	}
+	
 }
