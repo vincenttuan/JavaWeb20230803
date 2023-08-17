3cb947a1817df3ba4a3377ba3d48246c47cfe859
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-17 19:41:05 +0800

    web

---
+++ JavaWeb20230803/src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,43 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import service.UserService;
+
+@WebServlet(urlPatterns = "/servlet/login")
+public class LoginServlet extends HttpServlet {
+	
+	private UserService userService = new UserService();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 建立分派器
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/loginform.jsp");
+		rd.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 得到前端登入表單傳來的 username 與 password
+		String username = req.getParameter("usename");
+		String password = req.getParameter("password");
+		
+		// 判斷登入使否成功 ?
+		boolean check = userService.loginCheck(username, password);
+		
+		// 建立分派器
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/loginresult.jsp");
+		// 傳遞參數
+		req.setAttribute("username", username); // 登入者
+		req.setAttribute("check", check); // 是否登入成功 ?
+		rd.forward(req, resp);
+	}
+	
+	
+}
