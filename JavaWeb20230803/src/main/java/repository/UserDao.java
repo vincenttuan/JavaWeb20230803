e7a428f26d0bda5cf2cc65ac591855ba7a1e27ac
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-22 20:08:18 +0800

    web

--- JavaWeb20230803/src/main/java/repository/UserDao.java
+++ JavaWeb20230803/src/main/java/repository/UserDao.java
@@ -1,5 +1,8 @@
 package repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -8,17 +11,6 @@ import java.util.List;
 import repository.model.User;
 
 public class UserDao {
-	
-	// In-memory Database
-	private static List<User> users = new ArrayList<>();
-	
-	static {
-		users.add(new User(1, "john", "1234", getBirth(1954, 12, 8)));
-		users.add(new User(2, "mary", "5678", getBirth(1973, 12, 18)));
-		users.add(new User(3, "jack", "1111", getBirth(1982, 10, 10)));
-		users.add(new User(4, "rose", "2222", getBirth(1990, 8, 8)));
-		users.add(new User(5, "anna", "3333", getBirth(2002, 5, 3)));
-	}
 	
 	private static Date getBirth(int yyyy, int mm, int dd) {
 		Calendar calendar = Calendar.getInstance();
@@ -38,6 +30,28 @@ public class UserDao {
 	
 	// 查詢多筆
 	public List<User> queryAllUser() {
+		List<User> users = new ArrayList();
+		
+		String sql = "select id, username, password, birth from users";
+		try(Statement stmt = MySQL.getInstance().getConnection().createStatement();
+			ResultSet rs = stmt.executeQuery(sql)) {
+			
+			while (rs.next()) { // 將資料列逐筆取出 
+				// 將資料欄裡的資料依序取出
+				int id = rs.getInt("id");
+				String username = rs.getString("username");
+				String password = rs.getString("password");
+				Date birth = rs.getDate("birth");
+				// 並注入到 user 物件
+				User user = new User(id, username, password, birth);
+				// 加入到 users 集合中
+				users.add(user); 
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return users;
 	}
 	
