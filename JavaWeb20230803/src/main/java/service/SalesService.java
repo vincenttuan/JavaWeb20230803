41b48f96b0d65e2b63177de0f31832a07720d4ce
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-31 20:59:42 +0800

    web

--- JavaWeb20230803/src/main/java/service/SalesService.java
+++ JavaWeb20230803/src/main/java/service/SalesService.java
@@ -13,6 +13,11 @@ import repository.model.Sales;
 public class SalesService {
 	private SalesDao salesDao = new SalesDao();
 	
+	// 查詢所有 product
+	public List<String> findAllProduct() {
+		return salesDao.queryAllProduct();
+	}
+	
 	// 查詢全部資料
 	public List<Sales> findAll() {
 		return salesDao.queryAll();
