1b0c1784069f50d4ad5d5322d190ff8b0a45cce6
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-31 19:45:09 +0800

    web

---
+++ JavaWeb20230803/src/main/java/servlet/SalesAddServlet.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/servlet/sales/add")
+public class SalesAddServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/salesadd.jsp");
+		rd.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	
+}
