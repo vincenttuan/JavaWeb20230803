33255841692b5d5056d038f4ed6ff5cc33ef14a1
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-12 19:44:42 +0800

    web

--- JavaWeb20230803/src/main/java/filter/SalesFilter.java
+++ JavaWeb20230803/src/main/java/filter/SalesFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebFilter(value = {"/servlet/sales/*"})
 public class SalesFilter extends HttpFilter {
@@ -16,14 +17,21 @@ public class SalesFilter extends HttpFilter {
 	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		
-		// 判斷參數 pass=true 才要放行
-		String pass = request.getParameter("pass");
-		if(pass != null && pass.equals("true")) {
-			chain.doFilter(request, response); // 放行
+		// 判斷是否有登入的 session 屬性資料
+		HttpSession session = request.getSession(false);
+		// 取得 session 的登入狀態紀錄 login_status
+		if(session != null) { 
+			String login_status = session.getAttribute("login_status") + "";
+			if(login_status.equals("true")) {
+				chain.doFilter(request, response); // 放行
+			} else {
+				// 導向登入頁面給前端
+				request.getRequestDispatcher("/WEB-INF/jsp/salesloginform.jsp").forward(request, response);
+			}
 		} else {
-			response.getWriter().print("Stop");
+			// 導向登入頁面給前端
+			request.getRequestDispatcher("/WEB-INF/jsp/salesloginform.jsp").forward(request, response);
 		}
-		
 	}
 	
 }
