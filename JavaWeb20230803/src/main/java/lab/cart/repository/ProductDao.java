a4b327534b8af810d3e77edaa5993f65c92b1669
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-14 21:05:43 +0800

    web

--- JavaWeb20230803/src/main/java/lab/cart/repository/ProductDao.java
+++ JavaWeb20230803/src/main/java/lab/cart/repository/ProductDao.java
@@ -1,5 +1,36 @@
 package lab.cart.repository;
 
-public class ProductDao {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import lab.cart.repository.model.Product;
+import repository.MySQL;
+
+public class ProductDao {
+	
+	public List<Product> queryAll() {
+		String sql = "select id, product_code, product_name, product_qty, product_price from products";
+		List<Product> products = new ArrayList<>();
+		try(Statement stat = MySQL.getInstance().getConnection().createStatement();
+			ResultSet rs = stat.executeQuery(sql);) {
+			
+			while (rs.next()) {
+				Product product = new Product();
+				product.setId(rs.getInt("id"));
+				product.setProductCode(rs.getString("product_code"));
+				product.setProductName(rs.getString("product_name"));
+				product.setProductQty(rs.getInt("product_qty"));
+				product.setProductPrice(rs.getInt("product_price"));
+				products.add(product);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return products;
+	}
+	
 }
