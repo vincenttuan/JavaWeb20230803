419548af547cb1796f82ff7164d1c0a032220283
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-14 21:10:23 +0800

    web

--- JavaWeb20230803/src/main/java/lab/cart/controller/ProductServlet.java
+++ JavaWeb20230803/src/main/java/lab/cart/controller/ProductServlet.java
@@ -1,5 +1,31 @@
 package lab.cart.controller;
 
-public class ProductServlet {
+import java.io.IOException;
+import java.util.List;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lab.cart.repository.model.Product;
+import lab.cart.service.ProductService;
+
+@WebServlet(value = "/lab/cart")
+public class ProductServlet extends HttpServlet {
+	
+	private ProductService productService = new ProductService();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		List<Product> products = productService.findAll();
+		
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/lab/cart/product.jsp");
+		req.setAttribute("products", products);
+		rd.forward(req, resp);
+	}
+	
+	
+	
 }
