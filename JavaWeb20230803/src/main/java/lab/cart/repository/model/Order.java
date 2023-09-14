0c45561ef133899ca5dcb2fa9d2be0a2b8c95866
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-14 20:59:30 +0800

    java

--- JavaWeb20230803/src/main/java/lab/cart/repository/model/Order.java
+++ JavaWeb20230803/src/main/java/lab/cart/repository/model/Order.java
@@ -1,5 +1,57 @@
 package lab.cart.repository.model;
 
-public class Order {
+import java.util.Date;
 
+//對應的就是資料表 orders 的欄位
+public class Order {
+	private Integer id;
+	private Integer productId;
+	private Integer userId;
+	private Integer orderQty;
+	private Integer orderStatus;
+	private Date orderTs;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Integer getOrderQty() {
+		return orderQty;
+	}
+	public void setOrderQty(Integer orderQty) {
+		this.orderQty = orderQty;
+	}
+	public Integer getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(Integer orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	public Date getOrderTs() {
+		return orderTs;
+	}
+	public void setOrderTs(Date orderTs) {
+		this.orderTs = orderTs;
+	}
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", productId=" + productId + ", userId=" + userId + ", orderQty=" + orderQty
+				+ ", orderStatus=" + orderStatus + ", orderTs=" + orderTs + "]";
+	}
+	
+	
 }
--- JavaWeb20230803/src/main/java/lab/cart/repository/model/Product.java
+++ JavaWeb20230803/src/main/java/lab/cart/repository/model/Product.java
@@ -1,5 +1,48 @@
 package lab.cart.repository.model;
 
+// 對應的就是資料表 products 的欄位
 public class Product {
-
+	private Integer id;
+	private String productCode;
+	private String productName;
+	private Integer productQty;
+	private Integer productPrice;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getProductCode() {
+		return productCode;
+	}
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public Integer getProductQty() {
+		return productQty;
+	}
+	public void setProductQty(Integer productQty) {
+		this.productQty = productQty;
+	}
+	public Integer getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(Integer productPrice) {
+		this.productPrice = productPrice;
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", productCode=" + productCode + ", productName=" + productName + ", productQty="
+				+ productQty + ", productPrice=" + productPrice + "]";
+	}
+	
+	
 }
