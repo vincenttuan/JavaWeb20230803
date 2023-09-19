2b1cba18ca09a60fc9798e47e4e2177b4442f75d
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-19 20:51:18 +0800

    web

--- JavaWeb20230803/src/main/java/lab/cart/repository/ProductDao.java
+++ JavaWeb20230803/src/main/java/lab/cart/repository/ProductDao.java
@@ -12,7 +12,20 @@ import repository.MySQL;
 public class ProductDao {
 	
 	public List<Product> queryAll() {
-		String sql = "select id, product_code, product_name, product_qty, product_price from products";
+		//String sql = "select id, product_code, product_name, product_qty, product_price from products";
+		String sql = "SELECT "
+				+ "    p.id, "
+				+ "    p.product_code, "
+				+ "    p.product_name, "
+				+ "    (p.product_qty - COALESCE(o.total_order_qty, 0)) AS product_qty, "
+				+ "    p.product_price "
+				+ "FROM "
+				+ "    products p "
+				+ "LEFT JOIN "
+				+ "    (SELECT product_id, SUM(order_qty) AS total_order_qty FROM orders GROUP BY product_id) o "
+				+ "ON "
+				+ "    p.id = o.product_id;";
+		
 		List<Product> products = new ArrayList<>();
 		try(Statement stat = MySQL.getInstance().getConnection().createStatement();
 			ResultSet rs = stat.executeQuery(sql);) {
