a95f2997b15ad91591f3d4b832552c133efe1830
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-19 21:19:10 +0800

    web

--- JavaWeb20230803/src/main/java/lab/cart/controller/CartServlet.java
+++ JavaWeb20230803/src/main/java/lab/cart/controller/CartServlet.java
@@ -21,7 +21,25 @@ public class CartServlet extends HttpServlet {
 		HttpSession session = req.getSession(false);
  		int userId = Integer.parseInt(session.getAttribute("userid")+"");
  		
- 		// 取得商品 id
+ 		// 取得 action
+ 		String action = req.getParameter("action");
+ 		switch (action) {
+ 			case "view":
+ 				// 檢視購物車資料
+ 				
+				break;
+ 			default:
+ 				// 新增商品
+ 				addProduct(req, resp, userId);
+ 				break;
+		}
+ 		
+ 		
+	}
+	
+	// 新增商品
+	private void addProduct(HttpServletRequest req, HttpServletResponse resp, int userId) throws ServletException, IOException {
+		// 取得商品 id
  		int productId = Integer.parseInt(req.getParameter("product_id"));
  		
  		// 該使用者的購物車中是否有此商品 
@@ -35,6 +53,6 @@ public class CartServlet extends HttpServlet {
 		
  		//resp.getWriter().print("CartServlet OK, Please check database first~");
  		resp.sendRedirect(getServletContext().getContextPath() + "/lab/product");
-	}
+	} 
 	
 }
