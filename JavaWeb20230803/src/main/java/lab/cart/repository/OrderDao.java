3e06d838988431e782f405a720c23592b5e31d4e
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-19 19:26:32 +0800

    web

--- JavaWeb20230803/src/main/java/lab/cart/repository/OrderDao.java
+++ JavaWeb20230803/src/main/java/lab/cart/repository/OrderDao.java
@@ -27,4 +27,21 @@ public class OrderDao {
 		return false;
 	}
 	
+	// 新增商品到購物車中
+	public boolean addProductInCart(int userId, int productId, int qty) {
+		String sql = "insert into orders(user_id, product_id, order_qty, order_status) values(?, ?, ?, ?)";
+		try(PreparedStatement pstmt = MySQL.getInstance().getConnection().prepareStatement(sql)) {
+			pstmt.setInt(1, userId);
+			pstmt.setInt(2, productId);
+			pstmt.setInt(3, qty);
+			pstmt.setInt(4, 0); // 購物車 = 0, 已結帳 = 1
+			
+			int rowcount = pstmt.executeUpdate(); 
+			return rowcount >= 1;
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
 }
