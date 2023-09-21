a0abdb2f1fa8f7e6d8b6f7b7c0f37c46a0080c1f
MB-teacher <MB-teacher@MB-207-teacher>
2023-09-21 18:52:56 +0800

    web

--- JavaWeb20230803/src/main/java/lab/cart/service/ProductService.java
+++ JavaWeb20230803/src/main/java/lab/cart/service/ProductService.java
@@ -1,6 +1,7 @@
 package lab.cart.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import lab.cart.repository.ProductDao;
 import lab.cart.repository.model.Product;
@@ -9,8 +10,16 @@ public class ProductService {
 	
 	private ProductDao productDao = new ProductDao();
 	
+	// 查詢所有商品
 	public List<Product> findAll() {
 		return productDao.queryAll();
+	}
+	
+	// 查詢單一商品
+	public Product getProductById(int id) {
+		List<Product> products = productDao.queryAll();
+		Optional<Product> optProduct = products.stream().filter(p -> p.getId().equals(id)).findFirst();
+		return optProduct.isPresent() ? optProduct.get() : null;
 	}
 	
 }
